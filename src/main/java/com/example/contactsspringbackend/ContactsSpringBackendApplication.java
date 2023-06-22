fae4939b54c5bb15602a0bab5a312be90880a3d3
SamyakJ27 <samyakj27@gmail.com>
2023-06-22 08:50:35 -0700

    Created Project

---
+++ src/main/java/com/example/contactsspringbackend/ContactsSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactsspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/contactsspringbackend/ContactsSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactsspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
